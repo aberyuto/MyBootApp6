9daf45943697cced3709de65e623eb457f5a0ec2
190811 <190811@e0601-01.JC-21.local>
2022-06-03 15:03:36 +0900

    20220603課題13_1回目

--- src/main/java/jp/te4a/spring/boot/myapp13/repository/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp13/repository/BookRepository.java
@@ -1,8 +1,11 @@
-package jp.te4a.spring.boot.myapp12;
+package jp.te4a.spring.boot.myapp13.repository;
 
 
 
 import org.springframework.stereotype.Repository;
+
+import jp.te4a.spring.boot.myapp13.bean.BookBean;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<BookBean, Integer>{
